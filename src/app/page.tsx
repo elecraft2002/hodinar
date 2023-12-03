6b4e4782bed3804f21347232d51504ff57dc0b3a
Vojtech Suchanek <suchavo3@cvut.cz>
2023-12-03 20:57:28 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const page = await client.getSingle("homepage");
+  // const page = await client.getSingle("homepage");
+  const page = { data: { title: null, meta_description: "xdd" } };
   const settings = await client.getSingle("settings");
 
   return {
@@ -18,7 +20,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Home() {
   const client = createClient();
-  const page = await client.getSingle("homepage");
+  const page = await client.getSingle("homepage").catch(() => notFound());
   const watches = await client.getAllByType("watches");
 
   return (
