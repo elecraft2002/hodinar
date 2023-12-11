5d31b480978790ced14f288343e44dbb4fa02048
Vojtech Suchanek <suchavo3@cvut.cz>
2023-12-11 20:39:18 +0100

    Title

--- src/app/watches/[uid]/page.tsx
+++ src/app/watches/[uid]/page.tsx
@@ -44,9 +44,9 @@ export default async function Page({ params }: { params: Params }) {
               )} */}
             </div>
             <div className="md:flex-1 px-4">
-              <h2 className="text-2xl font-bold  mb-2">
+              <h1 className="text-2xl font-bold  mb-2">
                 {prismic.asText(page.data.title)}
-              </h2>
+              </h1>
 
               <div className="flex mb-4">
                 <div className="mr-4">
